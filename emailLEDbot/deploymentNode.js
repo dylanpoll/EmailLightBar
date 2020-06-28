f6fa963abf7928ef29ea4897e4d4b95a43c00dec
Dylan Poll <61894413+dylanpoll@users.noreply.github.com>
2020-06-28 03:54:44 -0400

    Delete deploymentNode.js

--- emailLEDbot/deploymentNode.js
+++
@@ -1,5 +0,0 @@
-const Run = require('./emailDB/emailConnect.js');         //the "model" for our "Post" object
-async function runNodeEmail(){await Run();}
-while(true){
-setTimeout(runNodeEmail,60000);
-}//this just deploys the emailer once every minute.
